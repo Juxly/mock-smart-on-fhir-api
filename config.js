017c7e6290f92bd609ca4c61c691ef251ae44c4c
James Ingold <jamesp.ingold@gmail.com>
2017-06-29 10:10:01 -0500

    adding dotenv and putting modulus into variable for deploying

--- config.js
+++ config.js
@@ -1,8 +1,11 @@
 import fs from 'fs'
+try {
+  require('dotenv').config()
+} catch (e) { console.log(e) }
 export default {
-  port: process.env.port || 3000,
-  secret: process.env.secret || 'BoringSecret',
+  port: process.env.PORT || 3000,
   mongoURL: process.env.MONGO_URL || 'mongodb://localhost:27017/juxly-fhir',
   key: fs.readFileSync('keys/private.key'),
-  publicKey: fs.readFileSync('keys/pubkey.pem')
+  publicKey: fs.readFileSync('keys/pubkey.pem'),
+  modulus: process.env.MODULUS
 }
--- routes/openid.js
+++ routes/openid.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import config from '../config'
 let router = express.Router()
 router.get('/.well-known/openid-configuration', function (req, res) {
   let config = {
@@ -24,7 +25,7 @@ router.get('/certs', function (req, res) {
       alg: 'RS256',
       use: 'sig',
       kid: '1',
-      n: Buffer.from('B37CB5BD628D14722A6AA5493206D359B46518BB841770E5F89EC4DECE2A0F22E8301CC615D0E0E88F67E7EB221E9A72DD5DA47B1F2ED0AB125AC589CF60CEBFB6CC5EE158039870935DC45E27440F724B7A799F7264B5ECC0D3EE138C09095704D7E7DB766C119DDD85230FABA771E405F8E9A919DD1D99BADEAD9E0A493C97', 'hex').toString('base64'),
+      n: Buffer.from(config.modulus, 'hex').toString('base64'),
       e: 'AQAB'
     }]
   }
